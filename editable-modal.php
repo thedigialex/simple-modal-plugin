ac2c2a75cb1c353653cf15b8b909b947b1dfcde2
thedigialex <132579975+thedigialex@users.noreply.github.com>
2024-03-23 16:36:40 -0400

    Update editable-modal.php

--- editable-modal.php
+++ editable-modal.php
@@ -66,8 +66,8 @@ function editable_modal_display_modal() {
     $button_class = get_option( 'button_class' );
     ?>
     <div id="Modal" class="modal" style="display: none; position: fixed; z-index: 1000; left: 0; top: 0; width: 100%; height: 100%; overflow: auto; background-color: rgba(0, 0, 0, 0.6);">
-      <div class="modal-content" style="background-color: #fefefe; margin: 5% auto; padding: 20px; border: 1px solid #888; width: 80%;">
-        <span class="close" style="color: #aaa; float: right; font-size: 28px; font-weight: bold; cursor: pointer;">&times;</span>
+      <div class="modal-content" style="margin: 5% auto; padding: 20px; width: 80%;">
+        <span class="close" style="float: right; font-size: 28px; font-weight: bold; cursor: pointer;">&times;</span>
         <h2><?php echo esc_html( $modal_title ); ?></h2>
         <div><?php echo do_shortcode( $modal_content ); ?></div>
       </div>
